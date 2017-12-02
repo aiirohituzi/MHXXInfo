9716348c148f0dae429a23d06b64e643c741776d
aiirohituzi <sinn5656@gmail.com>
2017-12-02 11:35:47 +0900

    Add Home and Quest vue. Disable es-lint.

---
+++ mhxxinfo-client/src/components/Quest.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h2>
+            {{ questName }}
+        </h2>
+        {{ contents }}
+        {{ condition }}
+        {{ reward }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Quest',
+    data () {
+        return {
+            questName: 'aa',
+            contents: 'dd',
+            condition: 'ss',
+            reward: 'cc'
+        }
+    }
+}
+</script>
+
+<style>
+</style>
