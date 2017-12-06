4e12886f472aa8ed8a6dc1547257f7d2d23abc37
aiirohituzi <sinn5656@gmail.com>
2017-12-06 21:35:57 +0900

    Create a webcrawler for data collection.

---
+++ crawler.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+import json
+
+chrome_options = Options()
+chrome_options.add_argument('headless')
+chrome_options.add_argument('--log-level=3')
+driver = webdriver.Chrome('./chromedriver/chromedriver', chrome_options=chrome_options)
+
+data = []
+
+ratings = {}
+
+driver.get('http://wiki.mhxg.org/data/2011.html')
+
+
+
+html = driver.page_source
+
+soup = BeautifulSoup(html, 'html.parser')
+
+
+
+for i in range(3, 8):
+    questName = soup.select(
+        '#quest21913' + str(i) + ' > div > div.panel-heading > a:nth-of-type(2)'
+    )
+    
+    questMap = soup.select(
+        '#quest21913' + str(i) + ' > div > div.panel-body.view_panel_body.a_cl21913' + str(i) + ' > a'
+    )
+    # print(questName[0].text)
+    data.append({
+        'questName': questName[0].text,
+        'questMap' : questMap[0].text
+    })
+
+data = json.dumps(data, indent=4)
+print(data)
+
+driver.close()
