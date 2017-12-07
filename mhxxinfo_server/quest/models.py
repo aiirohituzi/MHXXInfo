47744d33bf3ab8f1d7b1ab15fe8e64c4965cdf7b
aiirohituzi <sinn5656@gmail.com>
2017-12-07 21:20:44 +0900

    Edit quest model.

--- mhxxinfo_server/quest/models.py
+++ mhxxinfo_server/quest/models.py
@@ -4,10 +4,21 @@ from django.db import models
 class Quest(models.Model):
     questName = models.CharField(max_length=30)
 
-    rating = models.CharField(max_length=8, null=True)
+    rating = models.CharField(max_length=8)
     contents = models.CharField(max_length=200, null=True)
-    condition = models.CharField(max_length=50, null=True)
-    reward = models.CharField(max_length=300, null=True)
+    questMap = models.CharField(max_length=10)
+    questTime = models.CharField(max_length=5)
+
+    condition_main = models.CharField(max_length=50)
+    condition_sub = models.CharField(max_length=50, null=True)
+
+    down_payment = models.IntegerField()
+
+    rewardMoney_main = models.IntegerField()
+    rewardMoney_sub = models.IntegerField()
+
+    reward_main = models.CharField(max_length=300, null=True)
+    reward_sub = models.CharField(max_length=300)
 
     def __str__(self):
         return self.questName
