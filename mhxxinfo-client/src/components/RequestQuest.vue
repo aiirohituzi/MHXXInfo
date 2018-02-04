97ddbfae5be352a59c55acfe4687696931c26699
aiirohituzi <sinn5656@gmail.com>
2018-02-04 22:00:32 +0900

    Show 'villagers' request' by town.

--- mhxxinfo-client/src/components/RequestQuest.vue
+++ mhxxinfo-client/src/components/RequestQuest.vue
@@ -1,21 +1,100 @@
 <template>
     <div class="container">
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>의뢰명</th>
-                    <th>달성조건</th>
-                    <th>보상</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="row in requestQuest">
-                    <td>{{ row.requestName_kr }} {{ row.requestName }}</td>
-                    <td>{{ row.condition }}</td>
-                    <td>{{ row.reward }}</td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="panel panel-success">
+            <div class="panel-heading">베르나 마을(ベルナ村)</div>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>의뢰명</th>
+                        <th>달성조건</th>
+                        <th>보상</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="row in requestQuest" v-if="row.town=='베르나 마을(ベルナ村)'">
+                        <td>{{ row.requestName_kr }} {{ row.requestName }}</td>
+                        <td>{{ row.condition }}</td>
+                        <td>{{ row.reward }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="panel panel-success">
+            <div class="panel-heading">코코트 마을(ココット村)</div>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>의뢰명</th>
+                        <th>달성조건</th>
+                        <th>보상</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="row in requestQuest" v-if="row.town=='코코트 마을(ココット村)'">
+                        <td>{{ row.requestName_kr }} {{ row.requestName }}</td>
+                        <td>{{ row.condition }}</td>
+                        <td>{{ row.reward }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="panel panel-success">
+            <div class="panel-heading">폿케 마을(ポッケ村)</div>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>의뢰명</th>
+                        <th>달성조건</th>
+                        <th>보상</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="row in requestQuest" v-if="row.town=='폿케 마을(ポッケ村)'">
+                        <td>{{ row.requestName_kr }} {{ row.requestName }}</td>
+                        <td>{{ row.condition }}</td>
+                        <td>{{ row.reward }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="panel panel-success">
+            <div class="panel-heading">유쿠모 마을(ユクモ村)</div>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>의뢰명</th>
+                        <th>달성조건</th>
+                        <th>보상</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="row in requestQuest" v-if="row.town=='유쿠모 마을(ユクモ村)'">
+                        <td>{{ row.requestName_kr }} {{ row.requestName }}</td>
+                        <td>{{ row.condition }}</td>
+                        <td>{{ row.reward }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="panel panel-success">
+            <div class="panel-heading">그 외(その他)</div>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>의뢰명</th>
+                        <th>달성조건</th>
+                        <th>보상</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="row in requestQuest" v-if="row.town=='그 외(その他)'">
+                        <td>{{ row.requestName_kr }} {{ row.requestName }}</td>
+                        <td>{{ row.condition }}</td>
+                        <td>{{ row.reward }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
 
