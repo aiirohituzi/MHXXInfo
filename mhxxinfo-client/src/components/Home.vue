43fbf2d900f6fe5752c8f48e8977ee46edc4a327
DESKTOP-05FLCR5\sinn5 <sinn5656@gmail.com>
2018-03-15 22:31:40 +0900

    Received data output.

--- mhxxinfo-client/src/components/Home.vue
+++ mhxxinfo-client/src/components/Home.vue
@@ -6,6 +6,12 @@
                 <button class="btn btn-default" type="button" @click="allSearch(keyword)"><span class="glyphicon glyphicon-search"></span></button>
             </span>
         </div><br>
+
+        <div class="panel panel-info" v-if="this.detailData.active">
+            <div class="panel-heading">{{ this.detailData.title }}</div>
+            <div class="panel-body multiLine">{{ this.detailData.content }}</div>
+        </div>
+
         <div class="list-group">
             <a href="#" class="list-group-item" v-for="item in result" @click="detail(item.category, item.id)">{{item.result}}</a>
         </div>
@@ -21,6 +27,11 @@ export default {
         return {
             result: [],
             keyword: null,
+            detailData: {
+                'active': false,
+                'title': null,
+                'content': null,
+            },
         }
     },
     methods: {
@@ -39,21 +50,36 @@ export default {
                     break
                 case 'Kariwaza':
                     axios.get('http://localhost:8000/kariwazaById/?id=' + id).then((response) => {  
-                        console.log(response)
+                        // console.log(response.data)
+                        this.detailData.title = '수기 : ' + response.data[0].kariwazaName
+                        this.detailData.content = '종류 : ' + response.data[0].category + '\n' +
+                                                'LV : ' + response.data[0].level + '\n' + 
+                                                '개방 조건 : ' + response.data[0].condition
+                        this.detailData.active = true
                     }, (error) => {
                         console.log(error)
                     })
                     break
                 case 'Request':
                     axios.get('http://localhost:8000/requestQuestById/?id=' + id).then((response) => {  
-                        console.log(response)
+                        // console.log(response)
+                        this.detailData.title = '마을의뢰 : ' + response.data[0].requestName_kr + '(' + response.data[0].requestName + ')'
+                        this.detailData.content = '마을 : ' + response.data[0].town + '\n\n' +
+                                                '* 달성조건 *\n' + response.data[0].condition + '\n\n' +
+                                                '* 보상 *\n' + response.data[0].reward
+                        this.detailData.active = true
                     }, (error) => {
                         console.log(error)
                     })
                     break
                 case 'Skill':
                     axios.get('http://localhost:8000/skillById/?id=' + id).then((response) => {  
-                        console.log(response)
+                        // console.log(response)
+                        this.detailData.title = '스킬 : ' + response.data[0].skillType
+                        this.detailData.content = '* 발동 스킬명 *\n' + response.data[0].skillName + '\n\n' +
+                                                '* 필요 포인트 *\n' + response.data[0].point + '\n\n' +
+                                                '* 스킬 효과 *\n' + response.data[0].effect
+                        this.detailData.active = true
                     }, (error) => {
                         console.log(error)
                     })
@@ -66,4 +92,8 @@ export default {
 </script>
 
 <style>
+.multiLine {
+    white-space: pre-line;
+    text-align: left;
+}
 </style>
